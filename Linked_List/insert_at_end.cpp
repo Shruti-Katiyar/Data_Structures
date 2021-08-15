2a8c03b1326b60bf62272413bc1b79588b562e9f
Shruti-Katiyar <shrutikatiyar459@gmail.com>
2021-08-15 19:56:07 +0530

    insertion

---
+++ Linked_List/insert_at_end.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+using namespace std;
+class node{
+	public:
+		int data;
+		node*next;
+		node(int d): data(d),next(NULL){
+		}
+};
+void insert_at_end(node* &head,node* &tail,int data){
+	node*n =new node(data);
+	if(head==NULL){
+		head=tail=n;
+	}
+	else{
+		tail->next=n;
+		tail=n;
+	}
+}
+
+void printll(node*head){
+	while(head){
+		cout<<head->data<<"-->";
+		head=head->next;
+	}
+	cout<<"NULL"<<endl;
+}
+int main(){
+		
+		node*head=NULL,*tail=NULL;
+		
+		insert_at_end(head,tail,10);
+		insert_at_end(head,tail,20);
+		insert_at_end(head,tail,30);
+		insert_at_end(head,tail,40);
+		
+		printll(head);
+		
+		cout<<endl;
+		return 0;
+		
+		
+	}

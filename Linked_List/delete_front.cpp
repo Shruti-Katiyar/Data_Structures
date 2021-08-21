307ca703fc98cb2e81184a1a4dc084883387459b
Shruti-Katiyar <shrutikatiyar459@gmail.com>
2021-08-21 11:06:36 +0530

    delete

---
+++ Linked_List/delete_front.cpp
@@ -0,0 +1,62 @@
+#include<iostream>
+using namespace std;
+class node{
+	public:
+		int data;
+		node*next;
+		node(int d): data(d),next(NULL){
+		}
+};
+void insert_at_front(node* &head,node* &tail,int data){
+	node*n=new node(data);
+	if(head==NULL){
+		head=tail=n;
+	}
+	else{
+		n->next=head;
+		head=n;
+	}
+}
+void delete_at_front(node* &head,node* &tail){
+	if(head==NULL){
+		return;
+	}
+	else if(head->next==NULL){
+		delete head;
+		head=tail=NULL;
+	}
+	else{
+		node*n=head;
+		head=head->next;
+		delete n;
+	}
+}
+
+void printll(node*head){
+	while(head){
+		cout<<head->data<<"-->";
+		head=head->next;
+	}
+	cout<<"NULL"<<endl;
+}
+
+	int main(){
+		
+		node*head=NULL,*tail=NULL;
+		
+		insert_at_front(head,tail,1);
+		insert_at_front(head,tail,2);
+		insert_at_front(head,tail,3);
+		insert_at_front(head,tail,4);
+		
+		printll(head);
+		
+		delete_at_front(head,tail);
+	    delete_at_front(head,tail);
+	    printll(head);
+		
+		cout<<endl;
+		return 0;
+		
+		
+	}

f4eb03b1193f182b544638b88f6ffc25cabc331f
Shruti-Katiyar <shrutikatiyar459@gmail.com>
2021-09-08 13:06:21 +0530

    binarytree

---
+++ Binary Tree/binarytree.cpp
@@ -0,0 +1,79 @@
+#include<iostream>
+using namespace std;
+class node{
+public:
+		int data;
+		node*left;
+		node*right;
+		node(int d):data(d),left(NULL),right(NULL) {}
+		
+};
+node* Buildtree(){
+	int data;
+	cin>>data;
+	if(data==-1){
+		return NULL;
+	}
+	else{
+		node*root=new node(data);
+		root->left=Buildtree();
+		root->right=Buildtree();
+		return root;
+	}
+}
+void preorder(node*root){
+	//base case
+	if(root==NULL){
+		return;
+	}
+	//recursive case
+	cout<<root->data<<" ";
+	preorder(root->left);
+	preorder(root->right);
+}
+
+void inorder(node*root){
+	//base 
+	if(root==NULL){
+		return;
+	}
+	//recursive
+	
+	//1.print left
+	inorder(root->left);
+	//2.root
+	cout<<root->data<<" ";
+	//3.right
+	inorder(root->right);
+}
+
+void postorder(node*root){
+	//base 
+	if(root==NULL){
+		return;
+	}
+		
+	//recursive
+	
+	//1 left
+	postorder(root->left);
+	//2 right
+	postorder(root->right);
+	// root
+	cout<<root->data<<" ";
+}
+int main(){
+	node*root=Buildtree();
+	preorder(root);
+	cout<<endl;
+	inorder(root);
+	cout<<endl;
+	postorder(root);
+	cout<<endl;
+	
+	return 0;
+}
+//input
+// 8 3 1 -1 -1 6 4 -1 -1 7 -1 -1 10 -1 14 13 -1 -1 -1
+	
+
